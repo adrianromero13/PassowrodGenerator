35d99e88a7ab6800892a283cb954d58c7de85a3c
Adrian Romero <radrian031613@gmail.com>
2020-01-16 20:40:21 -0800

    first Commit with an html file, a css file, and a javascript file.

---
+++ scripts.js
@@ -0,0 +1,51 @@
+
+// request length
+let passwordLength = parseInt( prompt("Enter password length"));
+// lowercase?
+let lowerCase = confirm("Would you like lowercase characters?");
+// uppercase?
+let upperCase = confirm("Would you like uppercase characters?");
+// special characters?
+let specialChar = confirm("Would you like special characters?");
+// numbers?
+let numeric = confirm("Would you like numbers?");
+
+// password arrays
+let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "1234567890";
+let specialCharacters = "`~!@#$%^&*()";
+// set a variable for password and for valid characters
+
+let validCharacters = "";
+let password = "";
+
+// set if statements
+if (lowerCase) {
+    validCharacters += lowerCase;
+}
+if (upperCase) {
+    validCharacters += upperCase;
+}
+if (specialCharacters) {
+    validCharacters += specialCharacters;
+}
+if (numbers) {
+    validCharacters += numbers;
+}
+
+for (let i=0; i<passwordLength; i++) {
+    let index = Math.floor(Math.random()*validCharacters.length);
+    password += validCharacters[index]
+}
+console.log("Password", password);
+
+// display using HTML
+document.getElementById("passwordtextfield").innerHTML=password;
+
+function copypassword() {
+    let passwordText = document.querySelector("#passwordtextfield");
+    passwordText.select()
+    document.execCommand("copy")
+    alert("Your Password Has Been Copied");
+};
