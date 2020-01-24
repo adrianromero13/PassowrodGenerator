5677065e4dc321eaf10f0a9f8f02cfdf4ba8a439
Adrian Romero <radrian031613@gmail.com>
2020-01-23 21:09:35 -0800

    Added a readme.md file with placeholder text to modify. Made changes to the HTML to add style using bootstrap open source code. Decided to make some changes to the script.js commented out old code to make renovations

--- scripts.js
+++ scripts.js
@@ -1,4 +1,37 @@
+// needs improvement
 
+
+var length = parseInt( prompt("Enter password length"));
+// first challenge
+function password_generator( len ) {
+    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+    var numeric = '0123456789';
+    var punctuation = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    var password = "";
+    var character = "";
+    var crunch = true;
+    while( password.length<length ) {
+        entity1 = Math.floor(string.length * Math.random()*Math.random());
+        entity2 = Math.floor(numeric.length * Math.random()*Math.random());
+        entity3 = Math.floor(punctuation.length * Math.random()*Math.random());
+        hold = string.charAt( entity1 );
+        hold = (password.length)?(hold.toUpperCase()):(hold);
+        character += hold;
+        character += numeric.charAt( entity2 );
+        character += punctuation.charAt( entity3 );
+        password = character;
+    }
+    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+    return password;
+}
+
+console.log(password_generator());
+
+
+
+
+
+/*
 // request length
 let passwordLength = parseInt( prompt("Enter password length"));
 // lowercase?
@@ -14,7 +47,7 @@ let numeric = confirm("Would you like numbers?");
 let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "1234567890";
-let specialCharacters = "`~!@#$%^&*()";
+let specialCharacters = "`~!@#$%^&*";
 // set a variable for password and for valid characters
 
 let validCharacters = "";
@@ -38,10 +71,13 @@ for (let i=0; i<passwordLength; i++) {
     let index = Math.floor(Math.random()*validCharacters.length);
     password += validCharacters[index];
 }
-console.log("Password", password);
+// console.log("Password", password);
+*/
 
 // display using HTML
-document.getElementById("passwordtextfield").innerHTML=password;
+// document.getElementById("passwordtextfield").innerHTML=password;
+    // with challenge1
+document.getElementById("passwordtextfield").innerHTML=password_generator();
 
 function copypassword() {
     let passwordText = document.querySelector("#passwordtextfield");
